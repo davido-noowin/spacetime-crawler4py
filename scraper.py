89c15cf887637ae2153f52be48eb678d82d8c661
mgearhart <125161289+mgearhart@users.noreply.github.com>
2024-04-19 20:38:28 -0700

    Added robots.txt check and Relative to Absolute function

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 import re
 import shelve
 from utils.response import Response # located in the utils folder
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse, urljoin
 from bs4 import BeautifulSoup
+import urllib.robotparser
 
 
 def checkPath(url: str) -> bool:
@@ -12,11 +13,39 @@ def checkPath(url: str) -> bool:
     return True if re.search("(?:[a-z]+:)?//", url) else False
 
 
-def convertRelativeToAbosolute(url: str) -> str:
+def convertRelativeToAbsolute(url: str, possibleUrl: str) -> str:
     '''
     Converts a relative path to an absolute path so that we can crawl it
     '''
-    pass
+    return urljoin(url, possibleUrl)
+
+
+def checkRobotsTxt(url: str) -> bool:
+    '''
+    Sets robot parser object to robot.txt url, then checks if it can be crawled
+    '''
+    robotUrl = getRobotsUrl(url)
+    rp = urllib.robotparser.RobotFileParser()
+    rp.set_url(robotUrl)
+    try:
+        rp.read()
+        return rp.can_fetch("*", url)
+    except Exception as e:
+        print("Can't find robots.txt")
+        return False
+    
+    
+def getRobotsUrl(url):
+    '''
+    Attemps to replace url path with "robots.txt"
+    '''
+    try:
+        parsed_url = urlparse(url)
+        parsed_url = parsed_url._replace(path='/robots.txt')
+        return urlunparse(parsed_url)
+    except Exception as e:
+        print("Can't complete robots.txt url replacement")
+        return 
 
 
 def scraper(url: str, resp: Response) -> list:
@@ -25,6 +54,16 @@ def scraper(url: str, resp: Response) -> list:
 
 
 def extract_next_links(url, resp):
+    # Implementation required.
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    
     '''
     HOW TO APPROACH THIS FUNCTION:
 
@@ -37,15 +76,6 @@ def extract_next_links(url, resp):
     7. Choose the next URL to visit from the remaining list of URLs.
 
     '''
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
     print(f'\nDEBUG: url - {url} \nresponse url - {resp.url} \nresponse status - {resp.status} \nresponse error - {resp.error}\n')
     list_of_urls = []
 
@@ -54,14 +84,28 @@ def extract_next_links(url, resp):
         soup = BeautifulSoup(resp.raw_response.content, "html.parser")
         # print(soup) # prints HTML after being scraped
 
+        # check if robots.txt file says it's okay to crawl
+        if not checkRobotsTxt(resp.url):
+            print("This URL cannot be crawled due to robots.txt")
+            return []
+        else:
+            #print("DEBUG: ROBOTS SUCCESS")
+            pass
+
+
+
         for link in soup.find_all('a', href=True):
             possible_link = link['href']
             actual_link = ''
             if not checkPath(possible_link):
                 # if the path is not a valid absolute path then we manipulate it so that it becomes one
-                pass
-            actual_link = possible_link
+                #print("DEBUG POSSIBLE LINK:", possible_link)
+                actual_link = convertRelativeToAbsolute(url, possible_link)
+                #print("DEBUG ACTUAL LINK:", actual_link)
+            else:
+                actual_link = possible_link
             list_of_urls.append(actual_link)
+            
 
             # print(resp.url, '\n', link['href'], '\n', checkPath(link['href'])) # prints the links
             # TODO: turn all relative paths to absolute paths
@@ -70,7 +114,9 @@ def extract_next_links(url, resp):
 
     return list_of_urls
 
-
+        
+        
+        
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
