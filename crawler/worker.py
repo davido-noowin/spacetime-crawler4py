cfad8da0fc6099afec2f8fa41dc933de510ed62d
Peter Young <pyoung1284@gmail.com>
2024-04-23 18:33:12 -0700

    smarter delays

--- crawler/worker.py
+++ crawler/worker.py
@@ -6,6 +6,11 @@ from utils import get_logger
 import scraper
 import time
 
+#Peter: map domain to time last accessed for hopefully faster crawling
+from urllib.parse import urlparse
+from collections import defaultdict
+DOMAIN_LAST_ACCESSED = defaultdict(float)
+
 
 class Worker(Thread):
     def __init__(self, worker_id, config, frontier):
@@ -23,7 +28,14 @@ class Worker(Thread):
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
+
+            #Peter: map domain to time last accessed for hopefully faster crawling
+            domain = urlparse(tbd_url).netloc
+            if (wait_time := self.config.time_delay - (time.time() - DOMAIN_LAST_ACCESSED[domain])) >= 0.0:
+                time.sleep(wait_time)
             resp = download(tbd_url, self.config, self.logger)
+            DOMAIN_LAST_ACCESSED[domain] = time.time()
+
             self.logger.info(
                 f"Downloaded {tbd_url}, status <{resp.status}>, "
                 f"using cache {self.config.cache_server}.")
@@ -31,4 +43,5 @@ class Worker(Thread):
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+            #Peter: map domain to time last accessed for hopefully faster crawling
+            #time.sleep(self.config.time_delay)
