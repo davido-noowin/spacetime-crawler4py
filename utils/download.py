0ffcd693847bf86a43506bf7d7b2cb8ee270a8a7
axiang2 <axiang2@uci.edu>
2024-05-01 22:51:51 -0700

    commented out checkLinkSize

--- utils/download.py
+++ utils/download.py
@@ -9,27 +9,27 @@ MAX_REDIRECT = 10
 MAX_TIMEOUT_SECONDS = 10
 MAX_BYTE_SIZE = 1000000
 
-# determines whether or not to open a link based on its size
-def checkLinkSize(url: str, host, port, config) -> bool:
-    '''
-    Preliminary check to see if the url is too large.
-    Files that do not have a Content-Length header section will still try to be opened,
-    but if it takes too long then open will stop elsewhere.
-    Returns False if file is too large, otherwise True.
-    '''
-    response = requests.head(f"http://{host}:{port}/",
-            params=[("q", f"{url}"), ("u", f"{config.user_agent}")],
-            allow_redirects=True,               # allows for redirects
-            timeout=MAX_TIMEOUT_SECONDS)        # caps each download at a 20 second timeout
+# # determines whether or not to open a link based on its size
+# def checkLinkSize(url: str, host, port, config) -> bool:
+#     '''
+#     Preliminary check to see if the url is too large.
+#     Files that do not have a Content-Length header section will still try to be opened,
+#     but if it takes too long then open will stop elsewhere.
+#     Returns False if file is too large, otherwise True.
+#     '''
+#     response = requests.head(f"http://{host}:{port}/",
+#             params=[("q", f"{url}"), ("u", f"{config.user_agent}")],
+#             allow_redirects=True,               # allows for redirects
+#             timeout=MAX_TIMEOUT_SECONDS)        # caps each download at a 20 second timeout
     
-    size = response.headers.get('Content-Length')   # gets content length information from the header
+#     size = response.headers.get('Content-Length')   # gets content length information from the header
 
-    try:
-        size = int(size)
-    except:
-        return True     # if there is no Content-Length portion in the header, then we still crawl that link
-    else:
-        return False if size > MAX_BYTE_SIZE else True    # sets the threshold for the maximum size of a url to be 1MB
+#     try:
+#         size = int(size)
+#     except:
+#         return True     # if there is no Content-Length portion in the header, then we still crawl that link
+#     else:
+#         return False if size > MAX_BYTE_SIZE else True    # sets the threshold for the maximum size of a url to be 1MB
 
 
 def download(url, config, logger=None):
@@ -38,11 +38,11 @@ def download(url, config, logger=None):
         s = requests.Session()
         s.max_redirects = MAX_REDIRECT     # sets the max number of redirects that a link can have to 10
 
-        # if content is too large and exceeds a max size of 1MB, raise error
-        if not checkLinkSize(url, host, port, config):
-            raise Exception('File is too big')
+        # # if content is too large and exceeds a max size of 1MB, raise error
+        # if not checkLinkSize(url, host, port, config):
+        #     raise Exception('File is too big')
         
-        time.sleep(config.time_delay) #between HEAD and GET
+        # time.sleep(config.time_delay) #between HEAD and GET
 
         resp = requests.get(
             f"http://{host}:{port}/",
