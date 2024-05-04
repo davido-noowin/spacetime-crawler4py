0e1c30b6399972513a03b7053151ba87c559301b
David Nguyen <davido.nguyen1212@gmail.com>
2024-05-03 18:17:11 -0700

    viewDeliverables.py writes to txt files

--- viewDeliverables.py
+++ viewDeliverables.py
@@ -1,36 +1,53 @@
 import shelve
 
 def listUniquePages():
+    '''
+    Writes down the number of pages scraped
+    '''
     with shelve.open('unique_urls.db', 'r') as shelf:
-        print(len(shelf['unique_urls']))
+        with open('unique_urls.txt', 'w') as txt:
+            txt.write(str(len(shelf['unique_urls'])))
 
 
 def listMostWords():
+    '''
+    Finds the document with the largest number of words
+    '''
     with shelve.open('max_num_words.db', 'r') as shelf:
-        for key in shelf:
-            value = shelf[key]
-            print(f'Key: {key}, Value: {value}')
+        with open('max_num_words.txt', 'w') as txt:
+            for key in shelf:
+                value = shelf[key]
+                txt.write(f'{key}, {value}')
+                txt.write('\n')
 
 
 def listSubdomainCounts():
+    '''
+    Lists the subdomain counts of *.ics.uci.edu
+    '''
     with shelve.open('subdomain_counts.db', 'r') as shelf:
-        for key in shelf:
-            value = shelf[key]
-            print(f'Key: {key}, Value: {value}')
+        with open('subdomain_counts.txt', 'w') as txt:
+            for key in shelf:
+                value = shelf[key]
+                for item in value:
+                    txt.write(f'{item}, {value[item]}')
+                    txt.write('\n')
 
 
 def listWordFrequencies():
+    '''
+    Finds the words that have been used most frequently
+    '''
     with shelve.open('word_frequencies.db', 'r') as shelf:
-        for key in shelf:
-            value = shelf[key]
-            print(f'Key: {key}, Value: {value}')
+        with open('word_frequencies.txt', 'w') as txt:
+            sorted_word_freq = sorted(shelf.items(), key=lambda x: x[1], reverse=True)
+            for key, value in sorted_word_freq:
+                txt.write(f'{key}, {value}')
+                txt.write('\n')
 
 
-listUniquePages()
-print()
-listMostWords()
-print()
-listSubdomainCounts()
-print()
-listWordFrequencies()
-print()
+if __name__ == "__main__":
+    listUniquePages()
+    listMostWords()
+    listSubdomainCounts()
+    listWordFrequencies()
